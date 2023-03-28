6bb589a5705aedeff87327a773879e8904022bcc
thomas-gouder-ps <118989756+thomas-gouder-ps@users.noreply.github.com>
2023-03-28 10:13:17 +0100

    Update create.go

--- proxy/create.go
+++ proxy/create.go
@@ -48,7 +48,7 @@ func (i *IstioClient) createVirtualService(r route) error {
 	// This could be the case if the notebook pod is running in an other namespace than the hub pod
 	// and c.KubeSpawner.pod_connect_ip needs to be set to something like "jupyter-{username}.<target-namespace-of-notebook>.svc.cluster.local"
 	// Note: this is bypassed if the destinationHost is an IP address
-	if !strings.HasSuffix(destinationHost, fmt.Sprintf(".svc.%s", i.clusterDomain)) && !(strings.Count(destinationHost, ".") == 4) {
+	if !strings.HasSuffix(destinationHost, fmt.Sprintf(".svc.%s", i.clusterDomain)) && !(strings.Count(destinationHost, ".") == 3) {
 		destinationHost = fmt.Sprintf("%s.%s.svc.%s", destinationHost, i.namespace, i.clusterDomain)
 	}
 
